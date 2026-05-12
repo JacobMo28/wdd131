e8dea4dee43f7ce86767b9a8568cc91d7af6c8e3
JacobMo28 <mo24001@byui.edu>
2026-05-11 22:38:15 -0600

    week 4 day 1 ponder

---
+++ ponder/responsive.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+
+    nav.classList.toggle("hide");
+
+    menuButton.classList.toggle("change");
+
+});
